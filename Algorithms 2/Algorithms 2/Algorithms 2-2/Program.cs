6385d2dd7761d779d116bbebd2de15fcecce8e7f
David <davidgalamyan@gmail.com>
2021-01-20 17:47:09 +0300

    Complite HomeWork Algorithms 2

---
+++ Algorithms 2/Algorithms 2/Algorithms 2-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Algorithms_2_2
+{
+    class Program
+    {
+        public static int BinarySearch(int[] inputArray, int searchValue) // O(log n)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max) 
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    Console.WriteLine(mid);
+                    return  mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+            BinarySearch(array, 12);
+        }
+    }
+    
+}
---
+++ Algorithms 2/Algorithms 2/Algorithms 2/ILinkedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_2
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+    }              
+
+    public interface ILinkedList
+    {
+        int GetCount(); // Возращаем количество элементов в списке
+        void AddNode(int value); // добавляет новый элемент в конец списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+}
