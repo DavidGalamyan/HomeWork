91fe47a1b6cf9f6797e255bacb0d656fe34705ea
David <davidgalamyan@gmail.com>
2021-01-30 14:23:50 +0300

    HomeWork

---
+++ Algorithms 4/Algorithms 4 Core/Algorithms 4 Core/Algorithms 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.AddLeavesRec(100);
+            tree.AddLeavesRec(110);
+            tree.AddLeavesRec(108);
+            tree.AddLeavesRec(111);
+            tree.AddLeavesRec(50);
+            tree.AddLeavesRec(60);
+            tree.AddLeavesRec(45);
+            tree.AddLeavesRec(46);
+        }
+    }
+}
---
+++ Algorithms 4/Algorithms 4 Core/Algorithms 4 Core/Algorithms 4/Tree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_4
+{
+    public class Tree
+    {
+        public static Node Root;
+        private int count = 0;        
+        public void AddLeavesRec(int value)
+        {
+            if (Root == null)
+            {
+                Root = new Node { Data = value };
+                count++;
+            }
+            else
+            {
+                AddLeavesRec(value, Root);
+            }
+        }
+        private void AddLeavesRec(int value, Node node)
+        {
+            if (node.Data > value)
+            {
+                if (node.Left != null)
+                {
+                    AddLeavesRec(value, node.Left);
+                }
+                else
+                {
+                    node.Left = new Node { Data = value, Parent = node };
+                    count++;
+                }
+            }
+             if (node.Data < value)
+             {
+                if (node.Right != null)
+                {
+                    AddLeavesRec(value, node.Right);
+                }
+                else
+                {
+                    node.Right = new Node { Data = value, Parent = node };
+                    count++;
+                }
+             }            
+        }
+    }
+}
